cf892a92dff73c39823cde88be5e4a740a3e216e
averdin2 <averdin2@lion.lmu.edu>
2018-03-10 16:51:47 -0800

    added placeholder options menu

---
+++ Panel-Jumper-Game/Assets/Scripts/MainMenuBtnManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuBtnManager : MonoBehaviour {
+
+    public string newGameLevel;
+    public string optionsMenu;
+
+    private void Awake()
+    {
+        // Setting up runtime variables
+        float resAspect = 2f / 3f;
+        int screenHeight = Screen.height;
+        int screenWidth = (int)(screenHeight * resAspect);
+        Screen.SetResolution(screenWidth, screenHeight, true);
+        QualitySettings.SetQualityLevel(5);
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            NewGameBtn("Main_Game");
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitGameBtn();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            OptionsBtn("Options_Menu");
+        }
+
+    }
+
+    public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsBtn(string optionsMenu)
+    {
+        SceneManager.LoadScene(optionsMenu);
+    }
+}
--- Panel-Jumper-Game/Assets/Scripts/OptionsBtnManager.cs
+++ Panel-Jumper-Game/Assets/Scripts/OptionsBtnManager.cs
@@ -3,28 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOverBtnManager : MonoBehaviour {
+public class OptionsBtnManager : MonoBehaviour {
 
-    public string newGameLevel;
     public string mainMenu;
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            ReplayGameBtn("Main_Game");
-        }
-
         if (Input.GetKeyDown(KeyCode.M))
         {
             MainMenuBtn("Main_Menu");
         }
-    }
-
-    public void ReplayGameBtn(string newGameLevel)
-    {
-        SceneManager.LoadScene(newGameLevel);
     }
 
     public void MainMenuBtn(string mainMenu)
